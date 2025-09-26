bdceb872c34f43896dc916ec90c4fca1d4de1604
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 19:00:05 +0530

    Create Cwh89_Collection_hierarchy.java

---
+++ Java/Cwh89_Collection_hierarchy.java
@@ -0,0 +1,19 @@
+package company;
+
+//import java.util.ArrayList;
+//import java.util.Set;
+//import java.util.TreeSet;
+
+public class Cwh89_Collection_hierarchy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//ArrayList<E>
+		//Set<E>
+		//TreeSet<E>
+		
+	}
+
+}
