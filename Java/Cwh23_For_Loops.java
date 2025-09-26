7d205f93b237bc6947179699680648cd89f21b6d
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:45:33 +0530

    Create Cwh23_For_Loops.java

---
+++ Java/Cwh23_For_Loops.java
@@ -0,0 +1,49 @@
+package company1;
+
+public class Cwh23_For_Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// For loop
+		
+//		for(int i=1; i<=10 ; i++)
+//		{
+//			System.out.println(i);
+//		}
+
+	// Problem 1  // print odd number
+		
+	// odd Number :- 2n
+		
+	// Even Number :- 2n+1
+		
+//		System.out.println("This is Increamenting for loop");
+//		int n=3;
+//		for(int i=0 ; i<3 ; i++)
+//		{
+//		System.out.println(2*i+1);
+//		}
+		
+	// Decreamenting for loop
+		
+	//	for(int j = 5; j!=0; j--)
+		
+		System.out.println("This is Decreamenting for loop");
+		for(int j = 5; j>0; j--)
+		{
+			System.out.println(j);
+		}
+		
+	// Problem :- Print n natural number in reverse order
+		
+	System.out.println("Natural number in reverse order ");
+	
+		int n=10;
+		for(int k=n; k!=0; k--)
+		{
+			System.out.println(k);
+		}
+	}
+
+}
