235480c06c5a8ccb8055fc9dcf89e5153ccb10f1
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 19:18:40 +0530

    Update Cwh30_How_to_make_intellij_lookAmazinh.java

--- Java/Cwh30_How_to_make_intellij_lookAmazinh.java
+++ Java/Cwh30_How_to_make_intellij_lookAmazinh.java
@@ -1,6 +1,6 @@
 package company;
 
-public class CWH_30_How_to_make_intellij_lookAmazinh {
+public class CWH30_How_to_make_intellij_lookAmazinh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
