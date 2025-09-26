22702a2d6c6d71f0d2d583a316e086383e7aa017
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:42:48 +0530

    Create Cwh21_While_Loops.java

---
+++ Java/Cwh21_While_Loops.java
@@ -0,0 +1,50 @@
+package company1;
+
+public class Cwh21_While_Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		System.out.println(1);
+//		System.out.println(2);
+//		System.out.println(3);
+//		
+//		
+//		System.out.println("Using Loops");
+//		int i = 1;
+//		while(i<=3)
+//		{
+//			System.out.println(i);
+//			i++;
+//		}
+//		System.out.println("Finish while loop");
+//		
+//		while(true)  //  Infinite while loop
+//		{
+//			System.out.println("I am infinite while loop!");
+//		}
+		
+		
+// Problem 1 Natural number between 100 to 200.
+		
+		int j = 100;
+		while(j <= 200)
+		{
+			System.out.println(j);
+			j++;
+		}
+		System.out.println("End of the loop");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
