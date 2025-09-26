20412dcdc07fb66c226f7b56b569bd9cfd3c7c85
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:43:11 +0530

    Create Cwh22_Do_While_Loop.java

---
+++ Java/Cwh22_Do_While_Loop.java
@@ -0,0 +1,36 @@
+package company1;
+
+public class Cwh22_Do_While_Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// While loop
+		
+//		int a=0;
+//		while(a<5)
+//		{
+//			System.out.println(a);
+//			a++;
+//		}
+		
+		// Do While loop
+		
+	int b = 0;
+	do {
+		System.out.println(b);
+		b++;
+	}while(b<5);
+		
+		
+	// Problem 1  print natural no. upto 45
+	
+	int c = 1;
+	do {
+		System.out.println(c);
+		c++;;
+	}while(c<=45);
+	
+	}
+
+}
