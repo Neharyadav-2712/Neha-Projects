46cae73e629dd223e68b1b23acb2a6b507a86d88
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 19:19:46 +0530

    Update Cwh36_Introduction_to_oops.java

--- Java/Cwh36_Introduction_to_oops.java
+++ Java/Cwh36_Introduction_to_oops.java
@@ -1,6 +1,6 @@
 package company;
 
-public class Cwh_36_Introduction_to_oops {
+public class Cwh36_Introduction_to_oops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
