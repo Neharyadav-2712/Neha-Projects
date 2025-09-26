b0b0b07dff393e01888a580486187d5f9e5cb680
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:40:59 +0530

    Create Cwh13_Strings.java

---
+++ Java/Cwh13_Strings.java
@@ -0,0 +1,30 @@
+package company1;
+import java.util.*;
+
+public class Cwh13_Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		// String name = new String("Swati");
+		
+		String name = "Neha";
+		System.out.print("The name is: ");
+		System.out.println(name);
+		
+		
+		int a = 5;
+		float b = 7.347743f;
+		System.out.printf("The value of a is %d and the value of b is %10.2f",a,b);
+		//System.out.format("The value of a is %d and the value of b is %f",a,b);
+		
+	//	System.out.println("Enter the String");
+		Scanner sc = new Scanner(System.in);
+		
+		 String st = sc.nextLine();
+		 System.out.println("you Enter the String is given below");
+		 System.out.println(st);
+
+	}
+
+}
