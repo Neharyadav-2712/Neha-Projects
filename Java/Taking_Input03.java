0c97102c6e9ea4aee866a5497de8e15f0993966b
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:02:18 +0530

    Create Taking_Input03.java

---
+++ Java/Taking_Input03.java
@@ -0,0 +1,45 @@
+package company;
+import java.util.Scanner;
+
+public class Taking_Input03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Taking input from Uses");
+
+Scanner sc =  new Scanner(System.in);
+
+
+//1) For int
+System.out.println("Enter number 1");
+int a = sc.nextInt();
+System.out.println("Enter number 2");
+int b = sc.nextInt();
+
+int sum1 = a + b;
+
+
+System.out.println("The sum of number is");
+
+System.out.println(sum1);
+
+
+//2) For float
+System.out.println("Enter number 1");
+float a1 = sc.nextFloat();
+System.out.println("Enter number 2");
+
+float b1 = sc.nextFloat();
+
+
+float sum2 = a1 + b1;
+System.out.println("The sum of number is");
+
+System.out.println(sum2);
+
+
+
+	}
+
+}
