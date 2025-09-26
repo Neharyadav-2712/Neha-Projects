027f3886175aa5d1bacdd0a32ad0f3d298e17abd
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:11:05 +0530

    Update Cwh20_JavaExcercise2.java

--- Java/Cwh20_JavaExcercise2.java
+++ Java/Cwh20_JavaExcercise2.java
@@ -1,1 +1,39 @@
+package company;
+import java.util.Random;
+import java.util.*;
+
+public class Cwh_20_Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Qyes 1) rock paper scissors game
+		
+		
+		Random r= new Random();
+		
+		
+		int cMove; 
+		int userMove = 0; 
+		int cScore = 0; 
+		int pScore = 0; 
+		int tie = 0;
+		int rounds = 0; 
+		
+		
+		cMove = r.nextInt(1 , 4);
+		System.out.print("Number enter by computer is:");
+		System.out.println(cMove);
+		
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Number enter by user is  (between 1 to 3 , 1 for rock , 2 For paper , 3 scissors) ");
+		userMove = sc.nextInt();
+		
+
+		}
+	
+	}
+
+
 
