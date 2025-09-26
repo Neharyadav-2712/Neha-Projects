92240a709c581b16b056a915c4338373c0d73d99
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:45:24 +0530

    Create Cwh62_Chapter_12_Packages.java

---
+++ Java/Cwh62_Chapter_12_Packages.java
@@ -0,0 +1,28 @@
+package company;
+
+public class Cwh62_Chapter_12_Packages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	// Interpreter Vs compiler
+		
+	// Interpreter translates one statement at a time into machine code.	
+	// Compiler scans the entire program and translates whole of it into machine code.	
+		
+		
+		
+		
+		
+//		Interpreter                                             compiler
+
+		
+//		1) One statement at a time.                     Entire program at a time.
+//		2) Interpreter is needed everytime.				once compiled it is not needed.
+//		3) Partial execution if Error.					 No execution if an error occurs.
+//		4) Usually Easy for Programms.					 Usually not as easy interpreted ones.
+		
+	}
+
+}
