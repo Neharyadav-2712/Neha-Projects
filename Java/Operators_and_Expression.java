e8843e365efc5477f3c0892a4fd17304ffe8aa52
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:02:33 +0530

    Create Operators_and_Expression.java

---
+++ Java/Operators_and_Expression.java
@@ -0,0 +1,46 @@
+package company;
+
+public class Operators_and_Expression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Arithmatic  operator
+		
+		//int a = 4;
+		//int b = 6 * 2;
+		//System.out.println(b);
+		
+		// Multipication operator
+		//int a = 4;
+		//int b = 6 * 2;
+		//System.out.println(b);
+		
+		// Modular operator
+		//int a = 4;
+		//int b = 6 % 2; // Module operator
+		//System.out.println(b);
+		
+		// += operator
+		
+		int n = 3;
+		n += 3;
+		System.out.println(n); 
+		
+	// Increment & Decrement Operator
+		//int a =5;
+		//a++;
+//	System.out.println(a++);
+//	System.out.println(a);
+//	System.out.println(++a);
+//	
+//	System.out.println(a-- * 2);
+//	System.out.println(a);
+//	System.out.println(--a * 2);
+	}
+	
+	}
+	
+
+
