9db9e274b56dce18773b0502e3522e4914fcd4f7
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:02:55 +0530

    Create neha.java

---
+++ Java/neha.java
@@ -0,0 +1,21 @@
+package company;
+import company1.*;
+
+class c2 extends c1
+{
+	
+}
+
+
+public class neha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		c2 c = new c2();
+		
+		
+
+	}
+
+}
