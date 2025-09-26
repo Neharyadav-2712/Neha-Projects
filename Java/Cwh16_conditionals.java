eb1252b8b62b504ddd8cb532dd2bb2236a7940eb
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:56:50 +0530

    Create Cwh16_conditionals.java

---
+++ Java/Cwh16_conditionals.java
@@ -0,0 +1,36 @@
+package company;
+
+public class Cwh_16_conditionals {
+
+	public static void main(String[] args) {
+
+// 1)  If-else Statement
+		
+		
+// 		i) Not equals
+		int age = 6;
+		
+		if(age != 18)
+		{
+			System.out.println("You can drive");
+		}
+		else
+		{
+			System.out.println("you can not drive");
+		}
+		
+// 		i) greater than 
+		
+	int age1 = 6;
+		
+		if(age1 > 18)
+		{
+			System.out.println("You can drive");
+		}
+		else
+		{
+			System.out.println("you can not drive");
+		}
+		
+	}
+}
