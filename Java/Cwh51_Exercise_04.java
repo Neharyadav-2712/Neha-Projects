69d45e53fabafeaadb38ed78bcba4a6340ce7051
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:38:37 +0530

    Create Cwh51_Exercise_04.java

---
+++ Java/Cwh51_Exercise_04.java
@@ -0,0 +1,31 @@
+package company;
+
+public class Cwh51_Exercise_04 {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//  You have to implement a library using java class Library
+		//  Methods: addBook, issueBook, returnBook, showAvailableBooks
+		//  Properties: Array to store the available Books,
+		//  Array to store the issue Books, 
+		
+		
+	}
+
+}
