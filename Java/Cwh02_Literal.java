7464a45ceb84505c15d1e381557007f760b99ed5
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:03:44 +0530

    Create Cwh02_Literal.java

---
+++ Java/Cwh02_Literal.java
@@ -0,0 +1,24 @@
+package company;
+
+public class Literal02 {
+
+	public static void main(String[] args) {
+		
+byte age = 34;
+int age2 = 56;
+short age3 = 7554;
+long age4 = 34654365346335L;
+char ch = 'A';
+float f1 = 5.6f;
+double d1 = 5.55d;
+boolean a = true;
+String str ="ddfhfghfghhhhhhh45555555555555555555d";
+
+System.out.println(a);
+
+System.out.println(str);
+	}
+
+}
+
+
