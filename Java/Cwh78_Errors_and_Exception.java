b8cfd5318a66b822a7754fa72fc43d39d16809e2
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:35:16 +0530

    Create Cwh78_Errors_and_Exception.java

---
+++ Java/Cwh78_Errors_and_Exception.java
@@ -0,0 +1,19 @@
+package company1;
+
+public class Cwh78_Errors_and_Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Syntax error in Java 
+		
+		int a = 5;
+		b=3;
+		//System.out.println(a)
+		//System.out.println(a+b)
+		
+		
+		// Logical error in Java 
+	}
+
+}
