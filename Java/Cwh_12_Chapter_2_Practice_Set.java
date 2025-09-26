388e2bd859d8dc07309a12293bbb6cb27718f334
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:53:51 +0530

    Create Cwh_12_Chapter_2_Practice_Set.java

---
+++ Java/Cwh_12_Chapter_2_Practice_Set.java
@@ -0,0 +1,55 @@
+package company;
+
+import  java.util.*;
+
+public class Cwh_12_Chapter_2_Practice_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		// 1) what will be the result of following expression.
+		
+        //  float a = 7/4.0f *9/2.0f;
+        //  System.out.println(a);
+		
+		
+		// 2) Write a java program to encrypt a grade by adding 8 to it.Decrypt it to show the correct grade.
+		
+		
+		// For Encrypt
+		
+//		char Grade ='B';
+//	    Grade = (char)(Grade + 8);  //Use type casting convert in char
+//	    System.out.println(Grade);	
+		
+		// For Decrypt
+		
+//		Grade = (char)(Grade - 8);
+//		System.out.println(Grade);
+		
+		
+		// 3) Use comparison operators to find out whether a given number is greater than user entered number or not.
+//		
+//		Scanner sc = new Scanner(System.in);
+//
+//		int a = sc.nextInt();
+//	
+//		System.out.println(a>100);
+		
+		// 4) Write the following expression in java program.
+		
+//		float a = (((12f*3f)-(4f*4f))/(2f*1f*3f));
+//		System.out.println(a);
+		
+//		double a = (((12f*3f)-(4f*4f))/(2f*1f*3f));
+//		System.out.println(a);
+		
+		// 5) Find the value of the following expression.
+		
+		int x = 7;
+		int a = 7 *49/7 + 35/7;
+		System.out.println(a);
+		
+	}
+
+}
