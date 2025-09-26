9162b4d905f7869384b40a8066d8382870930e47
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:38:09 +0530

    Create Cwh95_HashSet.java

---
+++ Java/Cwh95_HashSet.java
@@ -0,0 +1,21 @@
+package company1;
+import java.util.HashSet;
+
+public class Cwh95_HashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashSet<Integer> h1 = new HashSet<>(6,0.5f);
+		
+		h1.add(5);
+		h1.add(62);
+		h1.add(7);
+		h1.add(48);
+		h1.add(29);
+		h1.add(10);
+		
+		System.out.println(h1);
+	}
+
+}
