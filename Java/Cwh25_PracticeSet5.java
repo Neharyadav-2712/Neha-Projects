373c797018af9356deec7d41b737aa75796c13cb
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 17:46:02 +0530

    Create Cwh25_PracticeSet5.java

---
+++ Java/Cwh25_PracticeSet5.java
@@ -0,0 +1,140 @@
+package company1;
+
+public class Cwh25_PracticeSet5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	// Problem 1
+				
+//		int n = 4;
+//		
+//		for(int i=n; i>0; i--)
+//		{
+//		for(int j= 0; j<i; j++)
+//		{
+//			System.out.print("*");
+//		}
+//			System.out.print("\n");
+//		}
+		
+//		int n = 4;
+//		
+//		for(int i= 0; i<n; i++)
+//		{
+//			for(int j=n; j>i; j--)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.print("\n");
+//		}
+		
+		
+	// Problem 2 
+		
+    // using while loop
+		
+//		int n = 3;
+//		int sum = 0;
+//		int i = 0;
+//		while(i<n)
+//		{
+//			sum += 2*i;
+//			i++;
+//		}
+//		System.out.println(sum);
+		
+	// Problem 3
+		
+//		int n=5;
+//		for(int i = 1 ; i<=10 ; i++)
+//		{
+//			System.out.printf("%d * %d = %d\n", n,i,n*i);
+//			
+//		}
+
+	// Problem 4
+		
+	//	int n =10;
+		//for(int i = 10 ; i>0 ; i--)        // OR
+		//for(int i = 10 ; i!=0 ; i--)       // OR
+	//		for(int i = 10 ; i>=1 ; i--)
+	//	{
+	//		System.out.printf("%d * %d = %d\n", n,i,n*i);
+			
+	//	}
+		
+	// Problem 5
+		
+//		int n = 5;
+//		int factorial = 1;
+//		for(int i=1; i<=n; i++)
+//		{
+//			factorial = factorial * i;
+//		}
+//		System.out.println(factorial);
+			
+	// Problem 6
+		
+//		int n = 5;
+//		int factorial = 1;
+//		int i = 1;
+//		while(i<=n)
+//		{
+//			factorial = factorial * i;
+//			i++;
+//		}
+//		System.out.println(factorial);
+	
+	// Problem 7
+			
+//		int n = 5;
+//		while(n>0)
+//		{
+//			int i = 0;
+//			while(i<n)
+//			{
+//				System.out.print("*");
+//				i++;
+//			}
+//			 System.out.print("\n");
+//			 
+//			 n--;
+//		}
+	
+	// Problem 8
+			
+	// True
+			
+	// Problem 9
+			
+		int n = 8;
+		int sum = 0;
+		for(int i = 1 ; i<=10 ; i++)
+		{
+			sum = sum + (n*i);
+		}
+		System.out.println(sum);
+		
+	// Problem 10
+				
+	// 1) At least once
+				
+	// Problem 11
+			
+	// using for loop	
+//	int n = 3;
+//	int sum = 0;
+//	for (int i=0 ; i<n ; i++)
+//	{
+//	sum = sum +(2*i);
+//	}
+//	System.out.println(sum);
+			
+	
+		
+		
+	}
+
+}
