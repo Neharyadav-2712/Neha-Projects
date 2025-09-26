7eeb744e5edcacddbd977f609be1fd2b1841280b
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 18:46:26 +0530

    Create Cwh65_Creating_packages_in_Java.java

---
+++ Java/Cwh65_Creating_packages_in_Java.java
@@ -0,0 +1,18 @@
+package company;
+//import java.util.Scanner;
+import java.util.*;
+
+
+public class Cwh_65_Creating_packages_in_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	Scanner sc = new Scanner(System.in);
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter a Number");
+		int a = sc.nextInt();
+		System.out.println("This is my scanner taking input as " + a);
+	}
+
+}
