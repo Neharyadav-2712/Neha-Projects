82c7e5e56894d9e874983d639479fadc612b8679
Neharyadav-2712 <neharyadav12@gmail.com>
2025-09-26 19:10:55 +0530

    Create Cwh24_break_and_continue.java

---
+++ Java/Cwh24_break_and_continue.java
@@ -0,0 +1,122 @@
+package company;
+
+public class Cwh24_break_and_continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Break and continue
+		// 1) Break 
+		
+//		for(int i=0; i<5; i++)
+//		{
+//			System.out.println(i);
+//			System.out.println("Java is great");
+//		
+//		if(i == 2)
+//		{
+//			System.out.println("Ending the loop");
+//			break;
+//		}
+//		}
+//		System.out.println("Loops end here");
+//		
+//		
+		
+		// break using while loop 
+		
+//		int i= 0;
+//		while(i<5)
+//		{
+//			System.out.println(i);
+//			System.out.println("Java is great");
+//			i++;
+//		
+//		if(i == 2)
+//		{
+//			System.out.println("Ending the loop");
+//			break;
+//		}
+//		
+//		}
+//		System.out.println("Loops end here");
+	
+		
+		// break using do while loop 
+		
+//		int i= 0;
+//		do
+//		{
+//			System.out.println(i);
+//			System.out.println("Java is great");
+//			i++;
+//		
+//		if(i == 2)
+//		{
+//			System.out.println("Ending the loop");
+//			break;
+//		}
+//		
+//	   }while(i<5);
+//		System.out.println("Loops end here");
+	
+		
+		
+		// 2) Continue Using for loop
+		
+//		for(int i=0; i<5; i++)
+//		{
+//			
+//		
+//		if(i == 2)
+//		{
+//			System.out.println("Ending the loop");
+//			continue;
+//		}
+//		System.out.println(i);
+//		System.out.println("Java is great");
+//		}
+//		System.out.println("Loops end here");
+		
+		
+// continue using while loop 
+		
+		int i= 0;
+		while(i<5){
+			if(i == 2){
+		System.out.println("Ending the loop");
+	continue;
+			}
+		
+		System.out.println(i);
+	System.out.println("Java is great");
+	i++;
+		}
+		System.out.println("Loops end here");
+	
+		
+		// continue using do while loop 
+		
+		
+		
+//		int i= 0;
+//		do
+//		{
+//			i++;
+//		if(i == 2)
+//		{
+//			System.out.println("Ending the loop");
+//			continue;
+//		}
+//		
+//
+//		System.out.println(i);
+//		System.out.println("Java is great");
+//
+//		
+//		}while(i<5);
+//		System.out.println("Loops end here");
+
+
+}
+	}
